9bbb6eca3b7f476585c00a7dc2d23b3d3c6d9046
James Beach <jbea599@aucklanduni.ac.nz>
2020-09-02 16:24:11 +1200

    STOR wokring

--- client/TCPClient.java
+++ client/TCPClient.java
@@ -1,3 +1,4 @@
+
 /**
  * Code is taken from Computer Networking: A Top-Down Approach Featuring 
  * the Internet, second edition, copyright 1996-2002 J.F Kurose and K.W. Ross, 
@@ -7,98 +8,131 @@
 import java.io.*;
 import java.net.*;
 import java.nio.file.FileSystems;
+import java.util.concurrent.TimeUnit;
 
-class TCPClient { 
+class TCPClient {
 
     private static File defaultDIR = FileSystems.getDefault().getPath("").toFile().getAbsoluteFile();
-    private static String dirString = defaultDIR.getPath(); 
-    
-    public static void main(String argv[]) throws Exception 
-    { 
-        String sentence; 
-        String response = "";
-	
-        BufferedReader inFromUser = 
-	    new BufferedReader(new InputStreamReader(System.in)); 
-	
-        Socket clientSocket = new Socket("localhost", 6789); 
-	
-        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream()); 
-	
-        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); 
+    private static String dirString = defaultDIR.getPath();
+    private static Socket clientSocket;
+    private static DataOutputStream outToServer;
+    private static DataOutputStream dataOutToServer;
+    private static BufferedReader inFromUser;
+    private static BufferedReader inFromServer;
+    private static BufferedInputStream dataInFromServer;
 
-        BufferedInputStream dataInFromServer = new BufferedInputStream(clientSocket.getInputStream());
-        
+    private static int character = 0;
+    private static String sentence;
+    private static String response = "";
+    private static String fileToSend;
 
+    public static void main(String argv[]) throws Exception {
 
-        int character = 0;
+        inFromUser = new BufferedReader(new InputStreamReader(System.in));
+        clientSocket = new Socket("localhost", 6789);
+        outToServer = new DataOutputStream(clientSocket.getOutputStream());
+        inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        dataInFromServer = new BufferedInputStream(clientSocket.getInputStream());
+        dataOutToServer = new DataOutputStream(clientSocket.getOutputStream()); // Used for Stor
+
         sentence = inFromServer.readLine();
         System.out.println(sentence);
 
-        while(true){
-            System.out.println(dirString);
-            System.out.println("Working Directory = " + System.getProperty("user.dir"));
-            sentence = inFromUser.readLine(); 
-            outToServer.writeBytes(sentence + '\n'); 
+        while (true) {
+            // System.out.println(dirString);
+            // System.out.println("Working Directory = " + System.getProperty("user.dir"));
+            sentence = inFromUser.readLine();
+            outToServer.writeBytes(sentence + '\n');
 
-            System.out.println("Waiting for response");
-            response = "";
-            while(true){
-                character = inFromServer.read();
-                if(character == 0){
-                    break;
-                }
-                response = response.concat(Character.toString((char)character));
+            getMessage();
+
+            if (sentence.contains("RETR") & (!response.contains("-"))) {
+                RETRlogic();
             }
 
-            System.out.println("FROM SERVER: " + response);
+            if (sentence.contains("STOR")) {
+                fileToSend = sentence.substring(9);
+                if (response.contains("+")) {
+                    File filets = new File(fileToSend);
+                    long filesize = filets.length();
+                    System.out.println(filesize);
+                    outToServer.writeBytes("SIZE " + filesize + '\n');
+                    getMessage();
+                    if (response.contains("+")) {
+                        System.out.println("Received send response, sending data");
+                        byte[] byteBuffer = new byte[(int) fileToSend.length()];
+                        int bytesCounter;
+                        FileInputStream fs = new FileInputStream(fileToSend);
+                        BufferedInputStream binaryStream = new BufferedInputStream(fs);
 
-            if(sentence.contains("RETR") & (!response.contains("-"))){
-                String requestedFile = sentence.substring(5);
-                System.out.println(requestedFile);
-                boolean validSend = false;
-                int sizefile = Integer.parseInt(response);
-                while(!validSend){
-                    System.out.println("inside valid send while loop, waiting to read from client ");
-                    sentence = inFromUser.readLine();
-                    if(sentence.equals("SEND") || (sentence.equals("STOP"))){
-                        validSend = true;
-                        System.out.println("valid send is true");
-                    }else{
-                        System.out.println("Not a valid message, send either SEND or STOP");
+                        while ((bytesCounter = binaryStream.read(byteBuffer)) >= 0) {
+                            dataOutToServer.write(byteBuffer, 0, bytesCounter);
+                        }
+                        binaryStream.close();
+                        fs.close();
+                        dataOutToServer.flush();
+                        TimeUnit.SECONDS.sleep(2);
+                        System.out.println("Finished sending");
+                        getMessage();
                     }
                 }
-                outToServer.writeBytes(sentence + '\n');
-
-                if(sentence.equals("SEND")){ // receive aborted message then will return to normal operation
-                    File file = new File(dirString + "\\" + requestedFile);
-                    System.out.println(file.getPath());
-                    // by default, files overwrite existing ones
-                    FileOutputStream fs = new FileOutputStream(file);
-                    BufferedOutputStream binaryOutputStream = new BufferedOutputStream(fs);
-                    for (int i = 0; i < sizefile; i++) {
-                        binaryOutputStream.write(dataInFromServer.read());
-                    }
-                    binaryOutputStream.close();
-                    fs.close();
-                    System.out.println("+A new file was received: " + requestedFile);
-                }
-                response = "";
-                while(true){
-                    character = inFromServer.read();
-                    if(character == 0){
-                        break;
-                    }
-                    response = response.concat(Character.toString((char)character));
-                }
-                System.out.println(response);
             }
 
-            if(response.equals("+localhost closing connection")){
+            if (response.equals("+localhost closing connection")) {
                 break;
             }
         }
-        clientSocket.close(); 
-	
-    } 
-} 
+        clientSocket.close();
+
+    }
+
+    public static void getMessage() throws IOException {
+        response = "";
+        while (true) {
+            character = inFromServer.read();
+            if (character == 0) {
+                break;
+            }
+            response = response.concat(Character.toString((char) character));
+        }
+
+        System.out.println("FROM SERVER: " + response);
+    }
+
+    public static void RETRlogic() throws IOException {
+        String requestedFile = sentence.substring(5);
+        System.out.println(requestedFile);
+        boolean validSend = false;
+        int sizefile = Integer.parseInt(response);
+        while (!validSend) {
+            System.out.println("inside valid send while loop, waiting to read from client ");
+            sentence = inFromUser.readLine();
+            if (sentence.equals("SEND") || (sentence.equals("STOP"))) {
+                validSend = true;
+                System.out.println("valid send is true");
+            } else {
+                System.out.println("Not a valid message, send either SEND or STOP");
+            }
+        }
+        outToServer.writeBytes(sentence + '\n');
+
+        if (sentence.equals("SEND")) {
+            RETRfile(requestedFile, sizefile);
+        }
+        getMessage();
+
+    }
+
+    public static void RETRfile(String requestedFile, int sizefile) throws IOException {
+        File file = new File(dirString + "\\" + requestedFile);
+        System.out.println(file.getPath());
+        FileOutputStream fs = new FileOutputStream(file);
+        BufferedOutputStream binaryOutputStream = new BufferedOutputStream(fs);
+        for (int i = 0; i < sizefile; i++) {
+            binaryOutputStream.write(dataInFromServer.read());
+        }
+        binaryOutputStream.close();
+        fs.close();
+        System.out.println("+A new file was received: " + requestedFile);
+    }
+}
--- server/TCPServer.java
+++ server/TCPServer.java
@@ -1,3 +1,4 @@
+
 /**
  * Code is taken from Computer Networking: A Top-Down Approach Featuring 
  * the Internet, second edition, copyright 1996-2002 J.F Kurose and K.W. Ross, 
@@ -11,118 +12,142 @@ import java.nio.file.*;
 import java.text.SimpleDateFormat;
 import java.util.concurrent.TimeUnit;
 
-class TCPServer { 
+class TCPServer {
 	static boolean loggedin = false;
 	static boolean userFound = false;
 	static boolean accFound = false;
 	static boolean passFound = false;
+	static boolean canSize = false;
+
 	static String parts[];
 	static String nameFile = "";
-	static int transferType = 1; // 1 corresponds to ASCII, 2 coressponds to binary and 3 to Continuous 
+	static int transferType = 2; // 1 corresponds to ASCII, 2 coressponds to binary and 3 to Continuous
 	private static File defaultDIR = FileSystems.getDefault().getPath("").toFile().getAbsoluteFile();
-	private static String dirString = defaultDIR.getPath(); 
-	static private DataOutputStream dataOutToClient;
+	private static String dirString = defaultDIR.getPath();
 
+	static File fileToStor;
+	static String fileToStorSTRING;
+
+	static Socket connectionSocket;
+	static ServerSocket welcomeSocket;
+
+	static String clientSentence;
+	static String response;
+
+	static BufferedReader inFromClient;
+	static DataOutputStream outToClient;
+	static DataOutputStream dataOutToClient;
+
+	static BufferedInputStream dataInFromClient;
 
 	public static void main(String argv[]) throws Exception {
-		String clientSentence;
 
-		ServerSocket welcomeSocket = new ServerSocket(6789);
+		welcomeSocket = new ServerSocket(6789);
+		System.out.println("Server started");
+
 		while (true) {
-			Socket connectionSocket = welcomeSocket.accept();
+			connectionSocket = welcomeSocket.accept();
 
-			BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+			inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+			outToClient = new DataOutputStream(connectionSocket.getOutputStream());
 
-			DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-			TCPServer.dataOutToClient = new DataOutputStream(connectionSocket.getOutputStream()); // create byte-based stream output
-			String response = "testds";
+			dataOutToClient = new DataOutputStream(connectionSocket.getOutputStream()); // Used for RETR
+			dataInFromClient = new BufferedInputStream(connectionSocket.getInputStream());
+
 			outToClient.writeBytes("+ Localhost SFTP service" + '\n');
 			while (true) {
-				System.out.println(dirString);
-				System.out.println("Working Directory = " + System.getProperty("user.dir"));
+				// System.out.println(dirString);
+				// System.out.println("Working Directory = " + System.getProperty("user.dir"));
 				clientSentence = inFromClient.readLine();
 
 				if (clientSentence.equals("DONE")) {
 					outToClient.writeBytes("+localhost closing connection");
-					TCPServer.RESET();
+					RESET();
 					break;
 
 				} else if (clientSentence.contains("USER")) {
-					response = TCPServer.USER(clientSentence.substring(5));
+					response = USER(clientSentence.substring(5));
 					System.out.println(defaultDIR);
 
 				} else if (clientSentence.contains("ACCT")) {
-					response = TCPServer.ACCT(clientSentence.substring(5));
+					response = ACCT(clientSentence.substring(5));
 
 				} else if (clientSentence.contains("PASS")) {
-					response = TCPServer.PASS(clientSentence.substring(5));
+					response = PASS(clientSentence.substring(5));
 
 				} else if (clientSentence.contains("TYPE")) {
-					if(TCPServer.loggedin == true){
-						response = TCPServer.TYPE(clientSentence.substring(5));
-					}else{
+					if (loggedin == true) {
+						response = TYPE(clientSentence.substring(5));
+					} else {
 						response = "! Cannot use this function until logged-in";
 					}
 				} else if (clientSentence.contains("LIST")) {
-					if(TCPServer.loggedin == true){
+					if (loggedin == true) {
 						ArrayList<String> ar = new ArrayList<String>();
 						String test[] = clientSentence.split(" ");
 
 						ar.add(test[0]);
 						ar.add(test[1]);
-						if(test.length == 2){
+						if (test.length == 2) {
 							ar.add("\0");
-						}else{
+						} else {
 							ar.add(test[2]);
 						}
-						response = TCPServer.LIST(ar.get(1).toString(), ar.get(2).toString(), outToClient);
-					}else{
+						response = LIST(ar.get(1).toString(), ar.get(2).toString());
+					} else {
 						response = "! Cannot use this function until logged-in";
 					}
 				} else if (clientSentence.contains("CDIR")) {
-					if(TCPServer.loggedin == true){
-						response = TCPServer.CDIR(clientSentence.substring(5));
-					}else{
+					if (loggedin == true) {
+						response = CDIR(clientSentence.substring(5));
+					} else {
 						response = "! Cannot use this function until logged-in";
 					}
 				} else if (clientSentence.contains("KILL")) {
-					if(TCPServer.loggedin == true){
-						response = TCPServer.KILL(clientSentence.substring(5));
-					}else{
+					if (loggedin == true) {
+						response = KILL(clientSentence.substring(5));
+					} else {
 						response = "! Cannot use this function until logged-in";
 					}
 				} else if (clientSentence.contains("NAME")) {
-					if(TCPServer.loggedin == true){
-						response = TCPServer.NAME(clientSentence.substring(5));
-					}else{
+					if (loggedin == true) {
+						response = NAME(clientSentence.substring(5));
+					} else {
 						response = "! Cannot use this function until logged-in";
 					}
-				} else if(clientSentence.contains("TOBE")){
-					if(TCPServer.loggedin == true){
-						response = TCPServer.TOBE(clientSentence.substring(5));
-					}else{
+				} else if (clientSentence.contains("TOBE")) {
+					if (loggedin == true) {
+						response = TOBE(clientSentence.substring(5));
+					} else {
 						response = "! Cannot use this function until logged-in";
 					}
 
 				} else if (clientSentence.contains("RETR")) {
-					if(TCPServer.loggedin == true){
-						response = TCPServer.RETR(clientSentence.substring(5), outToClient, inFromClient);
-					}else{
+					if (loggedin == true) {
+						response = RETR(clientSentence.substring(5));
+					} else {
 						response = "! Cannot use this function until logged-in";
 					}
 				} else if (clientSentence.contains("STOR")) {
-					if(TCPServer.loggedin == true){
-						response = TCPServer.STOR(clientSentence.substring(5));
-					}else{
+					if (loggedin == true) {
+						String test[] = clientSentence.split(" ");
+						response = STOR(test[1], test[2]);
+					} else {
+						response = "! Cannot use this function until logged-in";
+					}
+				} else if (clientSentence.contains("SIZE")) {
+					if (loggedin == true) {
+						response = SIZE(clientSentence.substring(5));
+					} else {
 						response = "! Cannot use this function until logged-in";
 					}
 				} else {
 					response = "Invalid Command. Please try again";
 				}
-				if(!(response.equals("NO RESPONSE"))){
+				if (!(response.equals("NO RESPONSE"))) {
 					System.out.println(response);
 					outToClient.writeBytes(response + '\0');
-				}else{
+				} else {
 					System.out.println("response already sent in function");
 				}
 			}
@@ -131,29 +156,29 @@ class TCPServer {
 
 	public static String USER(String input) throws FileNotFoundException {
 
-		if(input.equals("guest")){
+		if (input.equals("guest")) {
 			TCPServer.loggedin = true;
-			return("! guest logged in");
+			return ("! guest logged in");
 		}
 
-		File file=new File(dirString + "\\users.txt");    //creates a new file instance  
+		File file = new File(dirString + "\\users.txt"); // creates a new file instance
 		Scanner scanner = new Scanner(file);
 		int lineNum = 0;
-    	while (scanner.hasNextLine()) {
+		while (scanner.hasNextLine()) {
 			String line = scanner.nextLine();
 			lineNum++;
 			TCPServer.parts = line.split(" ");
 			System.out.println(parts[0] + " " + parts[1] + " " + parts[2]);
-			if(parts[0].contains(input)) {
+			if (parts[0].contains(input)) {
 				System.out.println("found the username on line: " + lineNum);
 				TCPServer.userFound = true;
 				break;
 			}
 		}
-		if(TCPServer.userFound == false){
-			return("- Invalid user-id, try again");
-		}else{
-			return("+ User-id valid, send account and password");
+		if (TCPServer.userFound == false) {
+			return ("- Invalid user-id, try again");
+		} else {
+			return ("+ User-id valid, send account and password");
 		}
 
 	}
@@ -162,64 +187,64 @@ class TCPServer {
 		System.out.println(parts[1]);
 		System.out.println(input);
 
-		if(parts[1].equals(input)) {
+		if (parts[1].equals(input)) {
 			TCPServer.accFound = true;
 		}
-		
-		if(TCPServer.accFound == false){
-			return("- Invalid account, try again");
-		}else if(TCPServer.accFound == true && TCPServer.passFound == false){
-			return("+ Account valid, send password");
-		}else{
+
+		if (TCPServer.accFound == false) {
+			return ("- Invalid account, try again");
+		} else if (TCPServer.accFound == true && TCPServer.passFound == false) {
+			return ("+ Account valid, send password");
+		} else {
 			TCPServer.loggedin = true;
-			return("! Account valid, logged-in");
+			return ("! Account valid, logged-in");
 		}
 	}
 
 	public static String PASS(String input) throws FileNotFoundException {
 		System.out.println(parts[2]);
 		System.out.println(input);
-		
-		if(parts[2].equals(input)) {
+
+		if (parts[2].equals(input)) {
 			TCPServer.passFound = true;
 		}
-		
-		if(TCPServer.passFound == false){
-			return("- Wrong password, try again");
-		}else if(TCPServer.passFound == true && TCPServer.accFound == false){
-			return("+ Send account");
-		}else{
+
+		if (TCPServer.passFound == false) {
+			return ("- Wrong password, try again");
+		} else if (TCPServer.passFound == true && TCPServer.accFound == false) {
+			return ("+ Send account");
+		} else {
 			TCPServer.loggedin = true;
-			return("! Logged-in");
+			return ("! Logged-in");
 		}
 	}
 
-	public static String TYPE(String input){
-		if(input.equals("a")){
+	public static String TYPE(String input) {
+		if (input.equals("a")) {
 			TCPServer.transferType = 1;
-			return("+ Using Ascii mode");
-		}else if(input.equals("b")){
+			return ("+ Using Ascii mode");
+		} else if (input.equals("b")) {
 			TCPServer.transferType = 2;
-			return("+ Using Binary mode");
-		}else if(input.equals("c")){
+			return ("+ Using Binary mode");
+		} else if (input.equals("c")) {
 			TCPServer.transferType = 3;
-			return("+ Using Continuous mode");
-		}else{
-			return("- Type not valid");
+			return ("+ Using Continuous mode");
+		} else {
+			return ("- Type not valid");
 		}
 
 	}
 
-	public static String LIST(String input, String dir, DataOutputStream outToClient) throws IOException {
-		
+	public static String LIST(String input, String dir) throws IOException {
+
 		File files[];
 		String response = "";
 		File filepath;
-		
-		if(dir.equals("\0")){
+
+		if (dir.equals("\0")) {
 			filepath = new File(dirString);
-		}else{ 	
-			filepath = new File(dirString + "\\" + dir); 
+		} else {
+			filepath = new File(dirString + "\\" + dir);
 		}
 
 		files = filepath.listFiles();
@@ -227,10 +252,10 @@ class TCPServer {
 		response = filepath.getAbsolutePath();
 		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy kk:mm");
 
-		if(input.equals("F") || input.equals("V")){
-			for(File file : files){
+		if (input.equals("F") || input.equals("V")) {
+			for (File file : files) {
 				response = response + '\n' + file.getName();
-				if(input.equals("V")){
+				if (input.equals("V")) {
 					long modifiedTime = file.lastModified();
 					String modifiedDate = dateFormat.format(new Date(modifiedTime));
 					response += "    " + modifiedDate + "    SIZE: " + file.length();
@@ -238,52 +263,52 @@ class TCPServer {
 				}
 
 			}
-		}else{
+		} else {
 			response = "- Invalid listing type, choose F or V";
 		}
-		return(response);
+		return (response);
 	}
 
-	public static String CDIR(String input){
-		
-		if(input.contains("~")){
+	public static String CDIR(String input) {
+
+		if (input.contains("~")) {
 			TCPServer.dirString = defaultDIR.getPath();
-			return("! Changed working dir to default");
-			
-		}else{
+			return ("! Changed working dir to default");
+
+		} else {
 			String newDir = TCPServer.dirString + "\\" + input;
 			System.out.println(newDir);
 			File newDirFILE = new File(newDir);
-			if(newDirFILE.isDirectory()){
+			if (newDirFILE.isDirectory()) {
 				TCPServer.dirString = newDir;
-				return("! Changed working dir to " + input);
-			}else{
-				return("- Can't connect to directory because: directory does not exist");
+				return ("! Changed working dir to " + input);
+			} else {
+				return ("- Can't connect to directory because: directory does not exist");
 			}
 		}
 	}
 
-	public static String KILL(String input){ 
+	public static String KILL(String input) {
 		File f = new File(dirString + "\\" + input);
 		boolean deleted;
 		try {
 			deleted = f.delete();
 		} catch (Exception e) {
-			return("Error");
+			return ("Error");
 		}
-		
-		if(deleted){
-			return("+ File deleted");
-		}else{
-			return("- Invalid file");
+
+		if (deleted) {
+			return ("+ File deleted");
+		} else {
+			return ("- Invalid file");
 		}
 	}
 
-	public static String NAME(String input){
+	public static String NAME(String input) {
 
 		String files[] = defaultDIR.list();
-		for(String s: files){
-			if(s.equals(input)){
+		for (String s : files) {
+			if (s.equals(input)) {
 				System.out.println(s + " " + input);
 				TCPServer.nameFile = input;
 				return ("+ File exists");
@@ -292,35 +317,34 @@ class TCPServer {
 		return ("- Can't find " + input);
 	}
 
-	public static String TOBE(String input){
+	public static String TOBE(String input) {
 		File file = new File(TCPServer.nameFile);
 
 		File file2 = new File(input);
 
-		if (file2.exists()){
-			return("- File  already exists");
+		if (file2.exists()) {
+			return ("- File  already exists");
 		}
 		boolean success = file.renameTo(file2);
-		if(success){
-			return(TCPServer.nameFile + " renamed to " + input);
-		}else{
-			return("- An unknown error occured");
+		if (success) {
+			return (TCPServer.nameFile + " renamed to " + input);
+		} else {
+			return ("- An unknown error occured");
 		}
 	}
-	
-	public static String RETR(String input, DataOutputStream outToClient, BufferedReader inFromClient) throws IOException,
-			InterruptedException {
+
+	public static String RETR(String input) throws IOException, InterruptedException {
 		File file1 = new File(input);
 		System.out.println("Inside RETR");
-		if(file1.exists()){
+		if (file1.exists()) {
 			long filesize = file1.length();
 			outToClient.writeBytes(String.valueOf(filesize) + '\0');
 			System.out.println("Waiting for response");
 			String cResponse = inFromClient.readLine();
-			if(cResponse.equals("SEND")){
+			if (cResponse.equals("SEND")) {
 				System.out.println("Received send response, sending data");
 				byte[] byteBuffer = new byte[(int) file1.length()];
-				int bytesCounter; 
+				int bytesCounter;
 				FileInputStream fs = new FileInputStream(file1);
 				BufferedInputStream binaryStream = new BufferedInputStream(fs);
 				while ((bytesCounter = binaryStream.read(byteBuffer)) >= 0) {
@@ -332,28 +356,87 @@ class TCPServer {
 				TimeUnit.SECONDS.sleep(2);
 				System.out.println("Finished sending");
 
-			}else{ 
+			} else {
 				System.out.println("received a stop response, finsihing");
-				return("+ ok, RETR aborted");
+				return ("+ ok, RETR aborted");
 			}
 
-
-		}else{
-			return("- File doesn't exist");
+		} else {
+			return ("- File doesn't exist");
 		}
 
-		return("Finished sending");
-	}
-	public static String STOR(String input){
-		return("");
-		
+		return ("Finished sending");
 	}
 
-	public static void RESET(){
+	public static String STOR(String input, String dir) {
+		System.out.println("Inside STOR");
+		fileToStor = new File(dir);
+
+		fileToStorSTRING = dir;
+		if (input.equals("NEW")) {
+			if (fileToStor.exists()) {
+				return ("- File exists, but system doesn't support generations");
+			} else {
+				canSize = true;
+				return ("+ File does not exist, will create a new file");
+			}
+
+		} else if (input.equals("OLD")) {
+			if (fileToStor.exists()) {
+				canSize = true;
+				return ("+ Will write over old file");
+			} else {
+				canSize = true;
+				return ("+ Will create a new file");
+			}
+
+		} else if (input.equals("APP")) {
+			return ("Havent done APP yet lol");
+		} else {
+			return ("Invalid command, send either NEW, OLD, or APP");
+		}
+
+	}
+
+	public static String SIZE(String input) throws IOException {
+		if (!(canSize)) {
+			return ("- Need to use STOR command before size command");
+		}
+		long filesize = Long.valueOf(input);
+		long drivesize = Files.getFileStore(defaultDIR.toPath().toRealPath()).getUsableSpace();
+		if (filesize > drivesize) {
+			return ("- Not enough room, dont send it");
+		} else {
+			outToClient.writeBytes("+ ok, waiting for file" + '\0');
+			File file = new File(dirString + "\\" + fileToStorSTRING);
+			System.out.println(file.getPath());
+			FileOutputStream fs = new FileOutputStream(file);
+			BufferedOutputStream binaryOutputStream = new BufferedOutputStream(fs);
+			System.out.println(filesize);
+			for (int i = 0; i < filesize; i++) {
+				System.out.println("test");
+				binaryOutputStream.write(dataInFromClient.read());
+			}
+			binaryOutputStream.close();
+			fs.close();
+			System.out.println("+A new file was received: " + fileToStorSTRING);
+			canSize = false;
+			return ("+ Saved " + fileToStorSTRING);
+
+		}
+
+	}
+
+	public static void RESET() {
 		TCPServer.userFound = false;
 		TCPServer.accFound = false;
 		TCPServer.passFound = false;
 		TCPServer.loggedin = false;
+		TCPServer.transferType = 1;
+		TCPServer.parts[0] = "";
+		TCPServer.parts[1] = "";
+		TCPServer.parts[2] = "";
+		canSize = false;
 	}
-		
-} 
+
+}
